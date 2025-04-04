98487d9c27ca39a8d0b14e22b704c12fdec51c54
aekus21 <aekus21@gmail.com>
2025-04-04 19:38:06 +0300

    подправленный тест проверки логина

--- test/test_login.py
+++ test/test_login.py
@@ -2,5 +2,7 @@
 
 
 def test_login(app):
+    if app.session.is_logged_in() is True:
+        app.session.logout()
     app.session.login_form('administrator', 'root')
     assert app.session.is_logged_in_as('administrator')
